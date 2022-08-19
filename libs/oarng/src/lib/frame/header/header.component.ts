d934d06a75044f8e989425e9af4a23097cb83aef
chuanlin2018 <c.lin@prometheuscomputing.com>
2022-08-19 11:11:38 -0400

    Made header text configurable

--- libs/oarng/src/lib/frame/header/header.component.ts
+++ libs/oarng/src/lib/frame/header/header.component.ts
@@ -6,11 +6,19 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+    title_line01: string = "MIDAS";
+    title_line02: string = "DATA PUBLISHING";
+
     @Input() appVersion: string = "1.0";
+    @Input() titleLn1: string = "MIDAS";
+    @Input() titleLn2: string = "DATA PUBLISHING";
+
 
     constructor() { }
 
     ngOnInit(): void {
+        this.title_line01 = this.titleLn1.toUpperCase();
+        this.title_line02 = this.titleLn2.toUpperCase();
     }
 
     goHome() {
