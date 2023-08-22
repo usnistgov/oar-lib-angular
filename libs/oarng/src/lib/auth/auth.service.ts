usnistgov/oar-lib-angular

commit 7ad761f8ad1723abfea7b40b246c9f74d0567364

auth.service: fix error handling

libs/oarng/src/lib/auth/auth.service.ts
@@ -203,15 +203,9 @@ export class OARAuthenticationService extends AuthenticationService {
                     this.loginUser(returnURL);
             }),
             catchError((e) => {
-                if (e.status && e.status == 401) {
-                    if (!nologin)
-                        // this will cause the browser to redirect to login service,
-                        // terminating this application
-                        this.loginUser(returnURL);
-
-                    return of(deepCopy(anonymousCreds));
-                }
-                return throwError(e);
+                if (e.status && e.status == 401) 
+                    return this.handleUnauthenticated((nologin) ? undefined : returnURL);
+                return this.handleFetchError(e);
             })
         );
     }
@@ -230,29 +224,54 @@ export class OARAuthenticationService extends AuthenticationService {
         console.debug("Authentication request url: ", url);
 
         return this.httpcli.get(url).pipe(
-            map<any, Credentials>(messageToCredentials),
-            catchError(this.handleError)
+            map<any, Credentials>(messageToCredentials)
         );
     }
 
     /**
-     * Handle the HTTP errors.
-     * @param error The error object.
-     * @returns An observable containing the error message.
+     * handle the case where the remote service indicates that the user is not logged in.  This 
+     * implementation will redirect the browser to the login site (via loginUser()) if requested;
+     * otherwise, it returns anonymous credentials.
+     * @param authReturnURL  if provided, the login process will be initiated; if this requires 
+     *                       browser redirection, this parameter will be interpreeted as the 
+     *                       URL to return to after completing the process.
+     * @return Observable<Credentials> 
      */
-    protected handleError(error: any) {
-        let errorMessage = '';
-        if (error.error instanceof ErrorEvent) {
-            // Get client-side error
-            errorMessage = error.error.message;
-        } else {
-            // Get server-side error
-            errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    handleUnauthenticated(authReturnURL?: string) : Observable<Credentials> {
+        if (authReturnURL) 
+            // this will cause the browser to redirect to login service,
+            // terminating this application
+            this.loginUser(authReturnURL);
+        return of(deepCopy(anonymousCreds));
+    }
+
+    /**
+     * Handle the possible errors while fetching Credentials.  The error could be 
+     * directly from the HTTP call (and is, thus, an HttpErrorResponse) or an error
+     * occuring while processing the response (making it a simple Error).  This
+     * implementation will interpret the type of error to report a helpful error message 
+     * to the consult, and then reraise the error.
+     * @param error The error object.
+     * @returns an Observable returned by throwError()
+     */
+    handleFetchError(error: any) {
+        if (error.status) {
+            let message = null;
+            if (error.status == 0) 
+                message = "Authentication Client/Communiction Error: " + error.error
+            else if (error.status >= 400 && error.status < 500) 
+                message = "Authentication service reports: " + error.message +
+                    "; incorrect service endpoint configuration?"
+            else if (error.status > 500)
+                message = "Authentication server error: " + error.message
+            else
+                message = "Unexpected Authentication service response: " + error.message
+            console.error(message);
         }
-        // window.alert(errorMessage);
-        return throwError(() => {
-            return errorMessage;
-        });
+        else 
+            console.error("Failure processing Authentication service response: "+error);
+
+        return throwError(error);
     }
                        
     /**

libs/oarng/src/lib/auth/auth.service.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed, waitForAsync  } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpErrorResponse } from '@angular/common/http';
 import { Configuration, CONFIG_URL } from '../config/config.model';
 import { ConfigurationService } from '../config/config.service';
 import { AuthModule, AuthenticationService, OARAuthenticationService } from './auth.module';
@@ -61,4 +62,44 @@ describe('AuthService', () => {
         
     });
 
+    /*
+    test("Should handle fetch errors well", async () => {
+        let svc = service as OARAuthenticationService;
+        expect.assertions(3);
+        try {
+            const c = await svc.fetchCredentialsFrom("goob://example.com/fake").toPromise();
+            throw new Error("Did not catch bad URL (cred: "+c+")");
+        }
+        catch (error) {
+            expect(error).toBeInstanceOf(Error);
+            expect((error as Error).message).toContain("goob");
+            expect((error as Error).message).not.toContain("Error Code");
+        }
+    });
+    */
+
+    test("handleError: processing error", async () => {
+        let svc = service as OARAuthenticationService;
+        let err = new Error("test error");
+        debugger;
+        expect.assertions(1);
+        try {
+            await svc.handleFetchError(err).toPromise();
+        }
+        catch (e) {
+            expect(e).toBe(err);
+        }
+    });
+
+    test("handleError: HTTP error", async () => {
+        let svc = service as OARAuthenticationService;
+        let err = new HttpErrorResponse({status: 404, statusText: "Not Found"});
+        expect.assertions(1);
+        try {
+            await svc.handleFetchError(err).toPromise();
+        }
+        catch (e) {
+            expect(e).toBe(err);
+        }
+    });
 });

libs/oarng/src/lib/auth/auth.ts
@@ -131,6 +131,8 @@ export interface AuthInfo {
  * convert an OAR authentication web service message to a Credentials object
  */
 export function messageToCredentials(message: AuthInfo) : Credentials {
+    if (! message.userDetails)
+        throw new Error("Unexpected authentication service response (missing userDetails): "+message);
     let out : Credentials = {
         userId: message.userDetails.userId,
         userAttributes: deepCopy(message.userDetails)

commit 823ba8eb8d3198c2f68963df1ac6c43b10d6cbdd

auth: fix fetchCredentials() broken in 7ad761f

libs/oarng/src/lib/auth/auth.service.ts
@@ -210,7 +210,7 @@ export class OARAuthenticationService extends AuthenticationService {
             catchError((e) => {
                 console.error("Credentials not available (status = "+e.status+")");
                 if (e.status && e.status == 401) 
-                    return this.handleUnauthenticated((nologin) ? undefined : returnURL);
+                    return this.handleUnauthenticated(!nologin, returnURL);
                 return this.handleFetchError(e);
             })
         );
@@ -243,8 +243,8 @@ export class OARAuthenticationService extends AuthenticationService {
      *                       URL to return to after completing the process.
      * @return Observable<Credentials> 
      */
-    handleUnauthenticated(authReturnURL?: string) : Observable<Credentials> {
-        if (authReturnURL) 
+    handleUnauthenticated(dologin: boolean, authReturnURL?: string) : Observable<Credentials> {
+        if (dologin) 
             // this will cause the browser to redirect to login service,
             // terminating this application
             if (this.loginUser(authReturnURL))