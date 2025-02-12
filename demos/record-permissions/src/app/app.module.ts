15545802c6aa4e2d8aa6e7e06cafec0e7a25a4bf
Niksa Blonder <niksa.blonder@nist.gov>
2025-02-12 17:15:10 -0500

    removed FrameModule as it is not being used

--- demos/record-permissions/src/app/app.module.ts
+++ demos/record-permissions/src/app/app.module.ts
@@ -4,7 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { FrameModule } from 'oarng';
+// us once the module is finished and oarng has been built
+// import { RecordPermissionsModule } from 'oarng';
+
+// use while testing and building demo app
 import { RecordPermissionsModule } from 'libs/oarng/src/lib/record-permissions/record-permissions.module';
 
 
@@ -15,7 +18,6 @@ import { RecordPermissionsModule } from 'libs/oarng/src/lib/record-permissions/r
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FrameModule,
     RecordPermissionsModule
 ],
   providers: [],
