c94542e202bf711bee203d3cd004f2bffa0e7d03
Niksa Blonder <niksa.blonder@nist.gov>
2025-02-13 16:21:10 -0500

    added MatAutocompleteModule

--- libs/oarng/src/lib/record-permissions/record-permissions.module.ts
+++ libs/oarng/src/lib/record-permissions/record-permissions.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatAutocompleteModule}  from '@angular/material/autocomplete';
+
 import { PermissionsWidgetComponent } from './permissions-widget/permissions-widget.component';
 
 @NgModule({
   
   imports: [
-    CommonModule
+    CommonModule,
+    MatAutocompleteModule
   ],
   declarations: [
     PermissionsWidgetComponent
