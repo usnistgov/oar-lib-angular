000dcca7ac977ea9a0705493545481c1cf00ce99
Ray Plante <raymond.plante@nist.gov>
2026-01-28 16:27:01 -0500

    rm unused modules dir (which can interfere with dependency installation)

--- modules/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    preset: 'jest-preset-angular',
-    setupFilesAfterEnv: ['./setup-jest.ts'],
-    moduleNameMapper: {
-        "^lodash-es$": "lodash"
-    },
-    globalSetup: 'jest-preset-angular/global-setup'
-};
